1e6780a39327b6a642ecf5526268f405728bcf77
Rhesa Davinanto <rhesa.davinanto@xiber.dev>
2023-04-25 00:59:03 +0700

    fixing some error

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,22 @@
 'use client'
+import Link from "next/link";
 import { useRouter } from "next/navigation";
 
 const page = () => {
-  // eslint-disable-next-line react-hooks/rules-of-hooks
-  const router = useRouter()
-  return router.push('/personal')
+  return (
+    <div className="flex flex-col items-center justify-center h-screen">
+      <h1 className="text-4xl font-bold mb-4">Welcome to my page!</h1>
+      <p className="text-lg text-gray-500 mb-8">
+        Click the button below to go to my personal page.
+      </p>
+      <Link
+        className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+        href='/personal'
+      >
+        Go to Personal Page
+      </Link>
+    </div>
+  );
 };
 
 export default page;
