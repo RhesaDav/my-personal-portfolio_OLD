6a70dcc273a2d73dd63c1cf7e17b21181cf83b62
Rhesa Davinanto <rhesa.davinanto@xiber.dev>
2023-04-25 12:44:05 +0700

    fixing style

--- app/personal/layout.tsx
+++ app/personal/layout.tsx
@@ -1,17 +1,17 @@
-'use client'
-import Navbar from "../../components/common/Navbar"
-import Footer from "../../components/common/Footer"
+"use client";
+import Navbar from "../../components/common/Navbar";
+import Footer from "../../components/common/Footer";
 
 export default function PersonalLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
-      <div>
-        <Navbar/>
-        {children}
-        <Footer/>
-      </div>
-  )
+    <div className="bg-gray-100">
+      <Navbar />
+      <div className="container px-10">{children}</div>
+      <Footer />
+    </div>
+  );
 }
